a841eaa7adf6b93e5f33e6ca1d13b3596666775d
Onderito <ulas.onder@outlook.fr>
2025-12-23 15:51:40 +0100

    test footer on mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import HowIWork from "./section/how-i-work";
 export default function Home() {
   return (
     <div>
-      <div className="">
+      <div>
         <HeroSection />
       </div>
       <div className="container">
